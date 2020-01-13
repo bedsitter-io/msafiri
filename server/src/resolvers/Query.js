807089d99bf98d0692d4e303474eb8c1b6310c15
alexmochu <mochualex4@gmail.com>
2020-01-13 17:12:39 +0300

    Filtering, Pagination & Sorting

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,7 +1,29 @@
-function feed(parent, args, context, info) {
-    return context.prisma.safaris()
-  }
+async function feed(parent, args, context, info) {
+    const where = args.filter ? {
+      OR: [
+        { description_contains: args.filter },
+        { title_contains: args.filter },
+      ],
+    } : {}
   
+    const safaris = await context.prisma.safaris({
+        where,
+        skip: args.skip,
+        first: args.first,
+        orderBy: args.orderBy
+        })  
+    const count = await context.prisma
+        .safarisConnection({
+        where,
+        })
+        .aggregate()
+        .count()
+    return {
+        safaris,
+        count,
+    }
+}
+
   module.exports = {
     feed,
   }
