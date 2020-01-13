85ef29ebb84ad9d69ce342769647aa51da05c9cd
alexmochu <mochualex4@gmail.com>
2020-01-13 10:28:26 +0300

    Connecting Server and Database with Prisma Client

--- server/src/index.js
+++ server/src/index.js
@@ -1,35 +1,28 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { prisma } = require('./generated/prisma-client')
 
-// 1
-let safaris = [{
-    id: 'safari-0',
-    title: '5 Days in the Mara',
-    description: 'Game Drive in the morning, bird watching in the evening'
-  }]
-
-let idCount = safaris.length
 const resolvers = {
     Query: {
         info: () => `This is the API of Msafiri`,
-        feed: () => safaris,
+        feed: (root, args, context, info) => {
+            return context.prisma.safaris()
+          },
     },
 
     Mutation: {
-        post: (parent, args) => {
-            const safari= {
-                id: `safari-${idCount++}`,
-                title: args.title,
-                description: args.description
-            }
-            safaris.push(safari)
-            return safari
-        }
-    }
+        post: (root, args, context) => {
+          return context.prisma.createSafari({
+            title: args.title,
+            description: args.description,
+          })
+        },
+      },
 }
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers,
+    context: { prisma },
 })
 
 server.start(() => console.log(`Server is running on http://localhost:4000`))
